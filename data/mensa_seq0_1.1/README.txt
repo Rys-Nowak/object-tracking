08e250a552a5e15500ad5737384ed08d7635909d
Eryk Mikołajek <mikolajek.eryk@gmail.com>
2024-06-11 15:35:13 +0200

    dataset added

---
+++ data/mensa_seq0_1.1/README.txt
@@ -0,0 +1,16 @@
+RGB-D People Dataset
+L. Spinello, M. Luber, K. Arras
+Univ. of Freiburg, 2011
+
+Annotated people and tracks in RGB-D Kinect data
+
+If you are considering using this data, please reference the following: 
+[1] L. Spinello, K. O. Arras. People Detection in RGB-D Data. IEEE Int. Conf. on Intelligent Robots and Systems (IROS), 2011.
+[2] M. Luber and L. Spinello and K. O. Arras People Tracking in RGB-D Data With On-line Boosted Target Models. IEEE Int. Conf. on Intelligent Robots and Systems (IROS), 2011
+
+
+
+
+Copyright:
+This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License. 
+This dataset is distributed WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
--- main.py
+++ main.py
@@ -8,37 +8,60 @@ import object_model
 
 if __name__ == '__main__':
 
-    path = 'data/pedestrian/input'
-    frames = []
+    path = 'data/mensa_seq0_1.1'
+    sequence_num = 0
+    frames_rgb = []
+    frames_depth = []
 
-    for i in range(970, 975):
-        im = cv2.imread(os.path.join(path, f'in{i:06d}.jpg'))
-        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-        im_resized = cv2.resize(im, (0, 0), None, 1, 1)
-        frames.append(im_resized)
+    for i in range(500):
+        im_rgb = cv2.imread(os.path.join(path, f'rgb/seq0_{i:04d}_{sequence_num}.ppm'))
+        # im_rgb = cv2.cvtColor(im_rgb, cv2.COLOR_BGR2RGB)
+        im_rgb = cv2.rotate(im_rgb, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        # im_resized = cv2.resize(im, (0, 0), None, 1, 1)
+        frames_rgb.append(im_rgb)
 
-    frames = frames[::-1]
+        im_depth = cv2.imread(os.path.join(path, f'depth/seq0_{i:04d}_{sequence_num}.pgm'), cv2.IMREAD_GRAYSCALE)
+        im_depth = cv2.rotate(im_depth, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        frames_depth.append(im_depth)
+
+        cv2.imshow('rgb', im_rgb)
+        cv2.imshow('depth', im_depth)
+        cv2.waitKey(1)
+
+
+    # frames = frames[::-1]
+
+    # path = 'data/pedestrian/input'
+    # frames = []
+    #
+    # for i in range(970, 975):
+    #     im = cv2.imread(os.path.join(path, f'in{i:06d}.jpg'))
+    #     im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    #     im_resized = cv2.resize(im, (0, 0), None, 1, 1)
+    #     frames.append(im_resized)
+    #
+    # frames = frames[::-1]
 
     # im1 = cv2.imread('data/lena.png')
     # im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
     
-    time1 = time.time()
-    
-    gran1, granulated_image1 = granulation.spatio_color_granules(frames[0], 50)
-    print("Spatio color granules: done")
-    gran2, granulated_image2 = granulation.spatio_temporal_granules(frames[0], frames[0:], 30)
-    print("Spatio temporal granules: done")
-    gran3, granulated_image3 = granulation.color_neighborhood_granules(gran2, frames[0], 50)
-    print("Color neighborhood granules: done")
-    
-    time2 = time.time()
-    # print(gran2)
-    
-    print("Elapsed time:", time2-time1)
-    
-    plt.imsave('spatio_color_granules.jpg', granulated_image1)
-    plt.imsave('spatio_temporal_granules.jpg', granulated_image2)
-    plt.imsave('color_neighborhood_granules.jpg', granulated_image3)
+    # time1 = time.time()
+    #
+    # gran1, granulated_image1 = granulation.spatio_color_granules(frames[0], 50)
+    # print("Spatio color granules: done")
+    # gran2, granulated_image2 = granulation.spatio_temporal_granules(frames[0], frames[0:], 30)
+    # print("Spatio temporal granules: done")
+    # gran3, granulated_image3 = granulation.color_neighborhood_granules(gran2, frames[0], 50)
+    # print("Color neighborhood granules: done")
+    #
+    # time2 = time.time()
+    # # print(gran2)
+    #
+    # print("Elapsed time:", time2-time1)
+    #
+    # plt.imsave('spatio_color_granules.jpg', granulated_image1)
+    # plt.imsave('spatio_temporal_granules.jpg', granulated_image2)
+    # plt.imsave('color_neighborhood_granules.jpg', granulated_image3)
 
     # current_frame = frames[0]
     # previous_frames = frames[0:]
@@ -55,10 +78,10 @@ if __name__ == '__main__':
     # plt.imshow(diffs[4])
     # plt.show()
 
-    time1 = time.time()
-    model_bottom, model_top = object_model.create_initial_object_model(frames[0], frames[1:])
-    print("Elapsed time object model:", time.time()-time1)
-
-    plt.imsave("top_object_model_estimation.jpg", model_top)
-    plt.imsave("bottom_object_model_estimation.jpg", model_bottom)
+    # time1 = time.time()
+    # model_bottom, model_top = object_model.create_initial_object_model(frames[0], frames[1:])
+    # print("Elapsed time object model:", time.time()-time1)
+    #
+    # plt.imsave("top_object_model_estimation.jpg", model_top)
+    # plt.imsave("bottom_object_model_estimation.jpg", model_bottom)
     
